ec5a3dc0e6160d1a864af1d6ed91f25ecb54c15a
gabriella rabelo <gabriellarabmonteiro@gmail.com>
2020-10-13 10:20:54 -0400

    feat: last changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { InMemoryDatabase } from './in-memory-database';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClient,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
   ],
   providers: [],
